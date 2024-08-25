251b878388f8b0ffe9ec4d696111c74266f34653
Chetan090302 <jettychetan524@gmail.com>
2024-08-25 19:17:31 +0530

    newly commited

--- Assignment/src/main/java/com/example/Assignment/TaskController/Taskcontroller.java
+++ Assignment/src/main/java/com/example/Assignment/TaskController/Taskcontroller.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping("/emp")
+@RequestMapping("/employee")
 public class Taskcontroller {
     @Autowired
     TaskService taskservice;
